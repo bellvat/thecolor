ebee2d041316b76cfd7c147e2680c34508370e99
Monica Sirathanarun <msirathanarun@gmail.com>
2017-08-17 06:48:46 +0000

    Updated link to movies in collection

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,8 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.paginate(:page => params[:page], :per_page => 4)
-    
-    @unique = Movie.distinct.pluck(:movie)
+    @all = Movie.all 
+    #Movie.distinct.pluck(:movie)
   end
 
   def create
@@ -11,11 +11,16 @@ class MoviesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+    @all = Movie.all 
+  end
+
 
   private
 
   def movie_params
-    params.require(:movie).permit(:movie, :month, :year, :thoughts)
+    params.require(:movie).permit(:moviename, :month, :year, :thoughts)
   end  
 
 end
